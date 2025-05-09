79b1f5e19b5ab7eda845cacf67f27a55ce5725c0
Goat3ggs <stefpandi12@gmail.com>
2025-05-09 20:59:04 +0300

    hopefully works

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/hangman-app/', // <- asta e cheia pentru GitHub Pages
   plugins: [react()],
 })
