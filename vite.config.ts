c3eabad7d30fb1b01f13a07c0bcadfe9525994be
Goat3ggs <stefpandi12@gmail.com>
2025-10-16 11:16:41 +0300

    add missing letter

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/hangman-app/', // <- asta e cheia pentru GitHub Pages
+  base: '/hangman-game/', // <- asta e cheia pentru GitHub Pages
   plugins: [react()],
 })
